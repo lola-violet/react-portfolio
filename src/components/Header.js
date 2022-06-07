1b042424abf8a0a63c7d83f45f9afd0fb12d4a40
Lola Applegate <laurenemilya@gmail.com>
2022-06-07 16:03:40 -0700

    added About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header({ currentPage, handlePageChange }) {
     return (
         <header className="box">
             <h1>Lola Applegate</h1>
-            <ul className="nav nav-tabs">
+            <ul className="nav nav-tabs justify-content-end">
                 <li className="nav-item">
                     <a href="#about"
                         onClick={() => handlePageChange('About')}
@@ -25,7 +25,7 @@ export default function Header({ currentPage, handlePageChange }) {
                     > Contact </a>
                 </li>
                 <li className="nav-item">
-                    <a href="./" target="_blank" className="nav-link"> Resume </a>
+                    <a href="../../public/Lola_Applegate_Resume.pdf" target="_blank" className="nav-link"> Resume </a>
                 </li>
             </ul>
         </header>
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+// About
+import React from 'react';
+
+export default function About() {
+    return (
+        <section>
+            <h1>About Me</h1>
+            <p>Full-stack web developer with 3+ years of digital marketing experience, a BBA in Management from Berkeley College New York, and a certificate in full-stack web development from the University of Washington's Professional & Continuing Education program. Aiming to leverage my background in marketing & management with newly developed skills in HTML, CSS, JavaScript, third-party APIs, and NodeJS.</p>
+            <p>Using an iterative build approach, I recently collaborated with 3 classmates, creating a trivia application that won the people's choice award (based on votes from all students in our class). As our team's Git Administrator, I used my background in organizational management to oversee our Github workflow, managing several branches & a virtual kanban board. I look forward to applying my new development skills in a collaborative professional environment with a focus on mobile-first & user-centered design.</p>
+        </section>
+    );
+}
