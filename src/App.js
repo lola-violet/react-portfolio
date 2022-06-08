75714a094d6e51e6c0068c0050dc50eb1932b8af
Lola Applegate <laurenemilya@gmail.com>
2022-06-07 17:31:58 -0700

    added footer with styling, linked icons, added styles to about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PortfolioContainer from './components/PortfolioContainer';
 import Footer from './components/Footer';
 import 'bootstrap';
+import './styles/app.css';
+
 
 function App() {
   return(
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import '../styles/footer.css';
 
 
 export default function Footer() {
     return (
         <footer>
-            <a href="mailto:lolaviolet.dev@gmail.com"> lolaviolet.dev@gmail.com </a>
-            <a href="https://github.com/lola-violet/" target="_blank" rel="noreferrer"> GitHub </a>
-            <a href="https://www.linkedin.com/in/lolaapplegate/" target="_blank" rel="noreferrer"> LinkedIn </a>
+            <div className="container">
+              <div className="custom-footer d-flex flex-wrap justify-content-center align-items-center" id="contact">
+                <a href="https://github.com/lola-violet" target="_blank"><i class="fa-brands fa-github fa-3x"></i></a>
+                <a href="mailto:lolaviolet.dev@gmail.com" target="_blank"><i class="fa-solid fa-envelope fa-3x"></i></a>
+                <a href="https://www.linkedin.com/in/lolaapplegate/" target="_blank"><i class="fa-brands fa-linkedin fa-3x"></i></a>
+              </div>
+            </div>
         </footer>
     )
 }
