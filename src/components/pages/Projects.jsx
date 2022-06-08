074cad358668bef33fd4ab02eea705cc97255975
Lola Applegate <laurenemilya@gmail.com>
2022-06-08 09:50:40 -0700

    styled projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,26 +1,57 @@
 // Projects
 import React from 'react';
 import 'bootstrap';
-import byobackpack from '../../images/byo_backpack.png'
+import '../../styles/projects.css';
+import byobackpack from '../../images/byo_backpack.png';
+import inspoJeopardy from '../../images/inspo-jeopardy.png';
+import weatherDash from '../../images/weather-dash.png';
 
 
 
 export default function Projects() {
     return (
         <div className='container mt-5'>
-            <div className='d-flex flex-wrap'>
-                <div className='card d-flex col-10 col-md-8 col-lg-4 border border-white'>
-                    <img src={byobackpack} alt='BYO Backpack Logo' />
+            <div className='d-flex flex-wrap justify-content-evenly'>
+                <div className='custom-card card d-flex col-10 col-md-8 col-lg-4 border border-white mx-1 mb-5'>
+                    <img src={byobackpack} alt='BYO Backpack Logo' className='card-img-top img-fluid' />
                     <div className='card-body'>
-                        <h3 className='card-title'>BYO Backpack</h3>
-                        <p className='card-text'>A full-stack collaborative event management application with CRUD functionality.</p>
+                        <h3 className='card-title custom-title'>BYO Backpack</h3>
                     </div>
+                    <div className='card-body custom-body'>
+                        <p className='card-text custom-desc'>A full-stack collaborative event management application with CRUD functionality.</p>
+                        <span className='custom-tech'>Handlebars | MySQL | JavaScript</span>
+                    </div>
+                    <div className='card-body d-flex justify-content-center'>
+                        <a href='https://byo-backpack.herokuapp.com/' target="_blank" rel="noreferrer" className='custom-link card-link'>Application</a>
+                        <a href='https://github.com/lola-violet/byo-backpack' target="_blank" rel="noreferrer" className='custom-link card-link'>Repository</a>
+                    </div>
+                </div>
+                <div className='custom-card card d-flex col-10 col-md-8 col-lg-4 border border-white mx-1 mb-5'>
+                    <img src={inspoJeopardy} alt='Inspirational Jeopardy Logo' className='card-img-top img-fluid' />
                     <div className='card-body'>
-                        <span>Handlebars | MySQL | JavaScript</span>
+                        <h3 className='card-title custom-title'>Inspirational Jeopardy</h3>
                     </div>
-                    <div className='card-body d-flex'>
-                        <button>Application</button>
-                        <button>Repository</button>
+                    <div className='card-body custom-body'>
+                        <p className='card-text custom-desc'>A group project using a combination of server-side APIs to create a Jeopardy-style trivia quiz.</p>
+                        <span className='custom-tech'>HTML | CSS | JavaScript</span>
+                    </div>
+                    <div className='card-body d-flex justify-content-center'>
+                        <a href='https://lola-violet.github.io/inspirational-jeopardy/' target="_blank" rel="noreferrer" className='custom-link card-link'>Application</a>
+                        <a href='https://github.com/lola-violet/inspirational-jeopardy' target="_blank" rel="noreferrer" className='custom-link card-link'>Repository</a>
+                    </div>
+                </div>
+                <div className='custom-card card d-flex col-10 col-md-8 col-lg-4 border border-white mx-1 mb-5'>
+                    <img src={weatherDash} alt='Weather Dashboard Logo' className='card-img-top img-fluid' />
+                    <div className='card-body'>
+                        <h3 className='card-title custom-title'>Weather Dashboard</h3>
+                    </div>
+                    <div className='card-body custom-body'>
+                        <p className='card-text custom-desc'>A web application using OneWeather API to provide current weather & forecast data for a city, based on user's search input.</p>
+                        <span className='custom-tech'>HTML | CSS | JavaScript</span>
+                    </div>
+                    <div className='card-body d-flex justify-content-center'>
+                        <a href='https://lola-violet.github.io/weather-dashboard/' target="_blank" rel="noreferrer" className='custom-link card-link'>Application</a>
+                        <a href='https://github.com/lola-violet/weather-dashboard' target="_blank" rel="noreferrer" className='custom-link card-link'>Repository</a>
                     </div>
                 </div>
             </div>
@@ -28,81 +59,3 @@ export default function Projects() {
     );
 }
 
-// {/* <div classNameName='d-flex flex-wrap justify-content-center' id='work'>
-// <div className="custom-card d-flex card col-10 col-md-8 col-lg-4 border border-white">
-//     <image src={byoBackpack} className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"></image>
-//     {/* <img src="assets/byo_backpack.png" className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"> */}
-//     <div className="card-body">
-//       <h3 className="custom-title card-title">BYO Backpack</h3>
-//       <p className="custom-card-text card-text text-white">A full-stack collaborative event management application with CRUD functionality.</p>
-//     </div>
-//     <div className="custom-tech">
-//         <span>Handlebars | MySQL | JavaScript</span>
-//     </div>
-//     <div className="card-body d-flex justify-content-around">
-//       <a href="https://byo-backpack.herokuapp.com/" target="_blank" rel="noreferrer" className="custom-card-link card-link">Application</a>
-//       <a href="https://github.com/lola-violet/byo-backpack" target="_blank" rel="noreferrer" className="custom-card-link card-link">Repository</a>
-//     </div>
-// </div>
-// </div> */}
-
-{/* <section>
-            <h1>My Projects</h1>
-            <div classNameName="relative-img">
-                <img classNameName="img" src="" alt="" />
-                <div classNameName="text-overlay project-name">
-                    <h2></h2>
-                    <h3>HTML/CSS/JavaScript</h3>
-                </div>
-                <div classNameName="center hidden">
-                    <a classNameName="text-overlay margin-right" href="" target="_blank" rel="noreferrer">GitHub
-                        Link</a>
-                    <a classNameName="text-overlay" href="" target="_blank" rel="noreferrer">Deployed
-                        Link</a>
-                </div>
-            </div>
-            <div classNameName="box">
-                <div classNameName="relative-img">
-                    <img classNameName="img" src="./images/weather-dashboard.png" alt="weather dashboard screen shot" />
-                    <div classNameName="text-overlay project-name">
-                        <h2>Weather Dashboard</h2>
-                        <h3>HTML/CSS/JavaScript</h3>
-                    </div>
-                    <div classNameName="center hidden">
-                        <a classNameName="text-overlay margin-right" href="https://github.com/chuanw101/weather-dashboard" target="_blank" rel="noreferrer">GitHub
-                            Link</a>
-                        <a classNameName="text-overlay" href="https://chuanw101.github.io/weather-dashboard/" target="_blank" rel="noreferrer">Deployed
-                            Link</a>
-                    </div>
-                </div>
-
-                <div classNameName="relative-img">
-                    <img classNameName="img" src="./images/workday-scheduler.png" alt="work day scheduler screen shot" />
-                    <div classNameName="text-overlay project-name">
-                        <h2>Work Day Scheduler</h2>
-                        <h3>HTML/CSS/JavaScript</h3>
-                    </div>
-                    <div classNameName="center hidden">
-                        <a classNameName="text-overlay margin-right" href="https://github.com/chuanw101/work-day-scheduler" target="_blank" rel="noreferrer">GitHub
-                            Link</a>
-                        <a classNameName="text-overlay" href="https://chuanw101.github.io/work-day-scheduler/" target="_blank" rel="noreferrer">Deployed
-                            Link</a>
-                    </div>
-                </div>
-
-                <div classNameName="relative-img">
-                    <img classNameName="img" src="./images/byob.png" alt="byo packpack screen shot" />
-                    <div classNameName="text-overlay project-name">
-                        <h2>BYO BackPack</h2>
-                        <h3>HandleBars/NodeJS</h3>
-                    </div>
-                    <div classNameName="center hidden">
-                        <a classNameName="text-overlay margin-right" href="https://github.com/chuanw101/byo-backpack" target="_blank" rel="noreferrer">GitHub
-                            Link</a>
-                        <a classNameName="text-overlay" href="https://byo-backpack.herokuapp.com/" target="_blank" rel="noreferrer">Deployed
-                            Link</a>
-                    </div>
-                </div>
-
-            </div>
-        </section> */}
