f3956f2213eb897af788daa1cb07a33f6d926db8
Lola Applegate <laurenemilya@gmail.com>
2022-06-07 17:45:57 -0700

    started styling project page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,67 +1,88 @@
 // Projects
 import React from 'react';
+import 'bootstrap';
+import byoBackpack from '../../images/byo_backpack.png';
+
 
 export default function Projects() {
     return (
-        <section>
+        <div classNameName='d-flex flex-wrap justify-content-center' id='work'>
+            <div className="custom-card d-flex card col-10 col-md-8 col-lg-4 border border-white">
+                <image src={byoBackpack} className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"></image>
+                {/* <img src="assets/byo_backpack.png" className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"> */}
+                <div className="card-body">
+                  <h3 className="custom-title card-title">BYO Backpack</h3>
+                  <p className="custom-card-text card-text text-white">A full-stack collaborative event management application with CRUD functionality.</p>
+                </div>
+                <div className="custom-tech">
+                    <span>Handlebars | MySQL | JavaScript</span>
+                </div>
+                <div className="card-body d-flex justify-content-around">
+                  <a href="https://byo-backpack.herokuapp.com/" target="_blank" rel="noreferrer" className="custom-card-link card-link">Application</a>
+                  <a href="https://github.com/lola-violet/byo-backpack" target="_blank" rel="noreferrer" className="custom-card-link card-link">Repository</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+{/* <section>
             <h1>My Projects</h1>
-            <div className="relative-img">
-                <img className="img" src="" alt="" />
-                <div className="text-overlay project-name">
+            <div classNameName="relative-img">
+                <img classNameName="img" src="" alt="" />
+                <div classNameName="text-overlay project-name">
                     <h2></h2>
                     <h3>HTML/CSS/JavaScript</h3>
                 </div>
-                <div className="center hidden">
-                    <a className="text-overlay margin-right" href="" target="_blank" rel="noreferrer">GitHub
+                <div classNameName="center hidden">
+                    <a classNameName="text-overlay margin-right" href="" target="_blank" rel="noreferrer">GitHub
                         Link</a>
-                    <a className="text-overlay" href="" target="_blank" rel="noreferrer">Deployed
+                    <a classNameName="text-overlay" href="" target="_blank" rel="noreferrer">Deployed
                         Link</a>
                 </div>
             </div>
-            <div className="box">
-                <div className="relative-img">
-                    <img className="img" src="./images/weather-dashboard.png" alt="weather dashboard screen shot" />
-                    <div className="text-overlay project-name">
+            <div classNameName="box">
+                <div classNameName="relative-img">
+                    <img classNameName="img" src="./images/weather-dashboard.png" alt="weather dashboard screen shot" />
+                    <div classNameName="text-overlay project-name">
                         <h2>Weather Dashboard</h2>
                         <h3>HTML/CSS/JavaScript</h3>
                     </div>
-                    <div className="center hidden">
-                        <a className="text-overlay margin-right" href="https://github.com/chuanw101/weather-dashboard" target="_blank" rel="noreferrer">GitHub
+                    <div classNameName="center hidden">
+                        <a classNameName="text-overlay margin-right" href="https://github.com/chuanw101/weather-dashboard" target="_blank" rel="noreferrer">GitHub
                             Link</a>
-                        <a className="text-overlay" href="https://chuanw101.github.io/weather-dashboard/" target="_blank" rel="noreferrer">Deployed
+                        <a classNameName="text-overlay" href="https://chuanw101.github.io/weather-dashboard/" target="_blank" rel="noreferrer">Deployed
                             Link</a>
                     </div>
                 </div>
 
-                <div className="relative-img">
-                    <img className="img" src="./images/workday-scheduler.png" alt="work day scheduler screen shot" />
-                    <div className="text-overlay project-name">
+                <div classNameName="relative-img">
+                    <img classNameName="img" src="./images/workday-scheduler.png" alt="work day scheduler screen shot" />
+                    <div classNameName="text-overlay project-name">
                         <h2>Work Day Scheduler</h2>
                         <h3>HTML/CSS/JavaScript</h3>
                     </div>
-                    <div className="center hidden">
-                        <a className="text-overlay margin-right" href="https://github.com/chuanw101/work-day-scheduler" target="_blank" rel="noreferrer">GitHub
+                    <div classNameName="center hidden">
+                        <a classNameName="text-overlay margin-right" href="https://github.com/chuanw101/work-day-scheduler" target="_blank" rel="noreferrer">GitHub
                             Link</a>
-                        <a className="text-overlay" href="https://chuanw101.github.io/work-day-scheduler/" target="_blank" rel="noreferrer">Deployed
+                        <a classNameName="text-overlay" href="https://chuanw101.github.io/work-day-scheduler/" target="_blank" rel="noreferrer">Deployed
                             Link</a>
                     </div>
                 </div>
 
-                <div className="relative-img">
-                    <img className="img" src="./images/byob.png" alt="byo packpack screen shot" />
-                    <div className="text-overlay project-name">
+                <div classNameName="relative-img">
+                    <img classNameName="img" src="./images/byob.png" alt="byo packpack screen shot" />
+                    <div classNameName="text-overlay project-name">
                         <h2>BYO BackPack</h2>
                         <h3>HandleBars/NodeJS</h3>
                     </div>
-                    <div className="center hidden">
-                        <a className="text-overlay margin-right" href="https://github.com/chuanw101/byo-backpack" target="_blank" rel="noreferrer">GitHub
+                    <div classNameName="center hidden">
+                        <a classNameName="text-overlay margin-right" href="https://github.com/chuanw101/byo-backpack" target="_blank" rel="noreferrer">GitHub
                             Link</a>
-                        <a className="text-overlay" href="https://byo-backpack.herokuapp.com/" target="_blank" rel="noreferrer">Deployed
+                        <a classNameName="text-overlay" href="https://byo-backpack.herokuapp.com/" target="_blank" rel="noreferrer">Deployed
                             Link</a>
                     </div>
                 </div>
 
             </div>
-        </section>
-    );
-}
+        </section> */}
