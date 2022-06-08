12d37fc36dfc0c69cde1e6cb23d241683afe05b9
Lola Applegate <laurenemilya@gmail.com>
2022-06-08 09:06:27 -0700

    fixed project page, structured cards

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,30 +1,50 @@
 // Projects
 import React from 'react';
 import 'bootstrap';
-import byoBackpack from '../../images/byo_backpack.png';
+import byobackpack from '../../images/byo_backpack.png'
+
 
 
 export default function Projects() {
     return (
-        <div classNameName='d-flex flex-wrap justify-content-center' id='work'>
-            <div className="custom-card d-flex card col-10 col-md-8 col-lg-4 border border-white">
-                <image src={byoBackpack} className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"></image>
-                {/* <img src="assets/byo_backpack.png" className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"> */}
-                <div className="card-body">
-                  <h3 className="custom-title card-title">BYO Backpack</h3>
-                  <p className="custom-card-text card-text text-white">A full-stack collaborative event management application with CRUD functionality.</p>
-                </div>
-                <div className="custom-tech">
-                    <span>Handlebars | MySQL | JavaScript</span>
-                </div>
-                <div className="card-body d-flex justify-content-around">
-                  <a href="https://byo-backpack.herokuapp.com/" target="_blank" rel="noreferrer" className="custom-card-link card-link">Application</a>
-                  <a href="https://github.com/lola-violet/byo-backpack" target="_blank" rel="noreferrer" className="custom-card-link card-link">Repository</a>
+        <div className='container mt-5'>
+            <div className='d-flex flex-wrap'>
+                <div className='card d-flex col-10 col-md-8 col-lg-4 border border-white'>
+                    <img src={byobackpack} alt='BYO Backpack Logo' />
+                    <div className='card-body'>
+                        <h3 className='card-title'>BYO Backpack</h3>
+                        <p className='card-text'>A full-stack collaborative event management application with CRUD functionality.</p>
+                    </div>
+                    <div className='card-body'>
+                        <span>Handlebars | MySQL | JavaScript</span>
+                    </div>
+                    <div className='card-body d-flex'>
+                        <button>Application</button>
+                        <button>Repository</button>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
+// {/* <div classNameName='d-flex flex-wrap justify-content-center' id='work'>
+// <div className="custom-card d-flex card col-10 col-md-8 col-lg-4 border border-white">
+//     <image src={byoBackpack} className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"></image>
+//     {/* <img src="assets/byo_backpack.png" className="card-img-top pt-3 img-fluid" alt="BYO Backpack Logo"> */}
+//     <div className="card-body">
+//       <h3 className="custom-title card-title">BYO Backpack</h3>
+//       <p className="custom-card-text card-text text-white">A full-stack collaborative event management application with CRUD functionality.</p>
+//     </div>
+//     <div className="custom-tech">
+//         <span>Handlebars | MySQL | JavaScript</span>
+//     </div>
+//     <div className="card-body d-flex justify-content-around">
+//       <a href="https://byo-backpack.herokuapp.com/" target="_blank" rel="noreferrer" className="custom-card-link card-link">Application</a>
+//       <a href="https://github.com/lola-violet/byo-backpack" target="_blank" rel="noreferrer" className="custom-card-link card-link">Repository</a>
+//     </div>
+// </div>
+// </div> */}
 
 {/* <section>
             <h1>My Projects</h1>
